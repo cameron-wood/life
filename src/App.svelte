259dce6acd786f70511d239b4bbfbd1b4657eeec
Cameron <cameronwood321@gmail.com>
2022-03-10 16:23:43 +0000

    add date of birth input field

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,36 @@
 <script lang="ts">
-  let expectancy: number = 80;
-  let age: number = 30 * 52;
+  const now = new Date();
+  const today = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
 
-  const weeks = (year: number, week: number) => {
-    return year * 52 + week
+  let expectancy: number = 80;
+  let dob: string = new Date().toISOString().split("T")[0];
+
+  let diffTime: number;
+  let diffWeeks: number;
+  $: diffTime = today.valueOf() - new Date(dob).valueOf();
+  $: diffWeeks = diffTime / (1000 * 60 * 60 * 24 * 7);
+
+  const fill = (year: number, week: number, ageWeeks: number): string => {
+    const weekNumber = year * 52 + week;
+
+    if (weekNumber < Math.floor(ageWeeks)) {
+      return "grey";
+    } else if (weekNumber < Math.ceil(ageWeeks)) {
+      return "lightgray";
+    } else {
+      return "none";
+    }
   }
 </script>
 
 <main>
+  <label for="dob">Date of Birth</label>
+  <input type="date" id="dob" bind:value={dob}>
   <h1>{expectancy}-year life in weeks</h1>
   <svg width="{52*15}" height="{expectancy*15}">
     {#each {length: expectancy} as _, year}
     {#each {length: 52} as _, week}
-      <rect x="{week*15}" y="{year*15}" width="10" height="10" stroke="grey" fill={weeks(year, week) < age ? "grey" : "none"} />
+      <rect x="{week*15}" y="{year*15}" width="10" height="10" stroke="darkgrey" fill={fill(year, week, diffWeeks)}/>
     {/each}
     {/each}
   </svg>
